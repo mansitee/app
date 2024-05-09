ae4bd1ab2deda5176083d151ee03d9acd7f4c42b
rusmansutardi <101523097+mansitee@users.noreply.github.com>
2024-05-10 02:21:11 +0700

    Update worker.js

--- a/worker.js
+++ a/worker.js
@@ -1,54 +1,104 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js");
 
-const CACHE = "pwabuilder-offline-page";
+workbox.setConfig({ debug: true });
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const {
+    routing: { registerRoute, setCatchHandler },
+    strategies: { CacheFirst, NetworkFirst, StaleWhileRevalidate },
+    cacheableResponse: { CacheableResponse, CacheableResponsePlugin },
+    expiration: { ExpirationPlugin, CacheExpiration },
+    precaching: { matchPrecache, precacheAndRoute },
+} = workbox;
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+precacheAndRoute([{ url: "/offline.html", revision: null }]);
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                // List of assets to be cached
+                "img/",
+                "img/android-chrome-36x36.png",
+                "img/android-chrome-48x48.png",
+                // Include other assets here...
+            ]);
+        })
+    );
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            Promise.all(
+                keys.map(key => {
+                    if (![cacheName].includes(key)) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.open(cacheName).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request).then(networkResponse => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        })
+    );
+});
+
+// Meminta izin notifikasi dari pengguna
+self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'notificationPermission') {
+        if (event.data.permission === 'granted') {
+            console.log('Notification permission granted.');
+            displayNotification();
+        }
+    }
+});
+
+// Fungsi untuk menampilkan notifikasi
+function displayNotification() {
+    self.registration.showNotification('New post available!', {
+        body: 'Check out the latest post on our blog.',
+        icon: '/path/to/icon.png' // Ganti dengan URL ikon notifikasi Anda
+    });
 }
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+// Mendaftarkan background sync saat service worker diaktifkan
+self.addEventListener('activate', event => {
+    // Mendaftarkan background sync
+    if ('serviceWorker' in navigator && 'SyncManager' in window) {
+        navigator.serviceWorker.ready
+            .then(registration => {
+                return registration.sync.register('sync-post');
+            })
+            .catch(error => {
+                console.error('Sync registration failed: ', error);
+            });
+    }
 });
+
+// Menangani background sync
+self.addEventListener('sync', event => {
+    if (event.tag === 'sync-post') {
+        event.waitUntil(syncPosts());
+    }
+});
+
+// Fungsi untuk melakukan sinkronisasi posting
+function syncPosts() {
+    return fetch('/sync-posts')
+        .then(response => {
+            console.log('Posts synced successfully.');
+        })
+        .catch(error => {
+            console.error('Error syncing posts: ', error);
+        });
+              }
